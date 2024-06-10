3d6a8e19a37c97088c97a81c9ed42e380de6c7c7
kybaq <flfsk12@gmail.com>
2024-06-10 21:29:43 +0900

    update: 로그인 / 회원가입 라우터 추가

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,17 +1,19 @@
-import React, { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import GlobalStyle from "../components/GlobalStyle";
-import AccountHome from "../pages/AccountHome";
-import AccountDetail from "../pages/AccountDetail";
+import GlobalStyle from "../GlobalStyle";
+import Home from "../pages/Home";
+import Detail from "../pages/Detail";
+import Login from "../pages/Login";
+import SignUp from "../pages/SignUp";
 
 function Router() {
   return (
     <BrowserRouter>
       <GlobalStyle />
       <Routes>
-        <Route path="/" element={<AccountHome />} />
-        <Route path="detail/:id" element={<AccountDetail />} />
-        {/* <Route /> */}
+        <Route path="/" element={<Home />} />
+        <Route path="/detail/:id" element={<Detail />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
